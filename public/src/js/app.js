c4edcf70fbc179a8b7632d1f3b6f09251551aac0
imkartikahuja <kartik.ahuja24@gmail.com>
2017-12-23 23:19:32 +0530

    basic caching added

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,16 +1,24 @@
+
 var deferredPrompt;
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-        .register('/sw.js')
-        .then(function () {
-            console.log("Service Worker registered");
-        });
+if (!window.Promise) {
+  window.Promise = Promise;
 }
 
-window.addEventListener('beforeinstallprompt',function (event) {
-   console.log('before install prompt fired');
-   event.preventDefault();
-   deferredPrompt = event;
-   return false;
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(function () {
+      console.log('Service worker registered!');
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
 });
